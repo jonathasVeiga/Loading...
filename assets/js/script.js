978a150ba5705eb0ba1f79904509e8f40008754e
jonathas Veiga <jonathas.webd@gmail.com>
2022-03-05 00:42:07 -0300

    adicionando video ao modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,17 +20,17 @@
 
  //O MODAL
 
- function autoPlayYouTubeModal() {
-    var triggerOpen = $("body").find('[data-tagVideo]');
-    triggerOpen.click(function() {
-      var theModal = $(this).data("bs-target"),
-        videoSRC = $(this).attr("data-tagVideo"),
-        videoSRCauto = videoSRC + "?autoplay=1";
-      $(theModal + ' iframe').attr('src', videoSRCauto);
-      $(theModal + ' button.btn-close').click(function() {
-        $(theModal + ' iframe').attr('src', videoSRC);
-      });
-    });
-  }
+//  function autoPlayYouTubeModal() {
+//     var triggerOpen = $("body").find('[data-tagVideo]');
+//     triggerOpen.click(function() {
+//       var theModal = $(this).data("bs-target"),
+//         videoSRC = $(this).attr("data-tagVideo"),
+//         videoSRCauto = videoSRC + "?autoplay=1";
+//       $(theModal + ' iframe').attr('src', videoSRCauto);
+//       $(theModal + ' button.btn-close').click(function() {
+//         $(theModal + ' iframe').attr('src', videoSRC);
+//       });
+//     });
+//   }
   
 
