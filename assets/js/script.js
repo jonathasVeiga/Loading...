b163043d1b4078c56291f67b7b40a815d14a7a28
jonathas Veiga <jonathas.webd@gmail.com>
2022-03-13 11:52:38 -0300

    Adicionando icone bootstrap

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,34 @@
  //O MENU
-//  const $menu = document.querySelectorAll('#menu');
-//  const $a = document.querySelectorAll("#link");
+ const $menu = document.querySelectorAll('#menu');
+ const $a = document.querySelectorAll("#link");
  
-//  const $div = document.getElementById('navbarNavDropdown');
-//  const $button = document.getElementById('btn');
+ const $div = document.getElementById('navbarNavDropdown');
+ const $button = document.getElementById('btn');
 
-//  $menu.forEach($coin => $coin.addEventListener('click',handleAddCLass));
-//  $a.forEach($coin => $coin.addEventListener('click',handleAddCLass));
+ $menu.forEach($coin => $coin.addEventListener('click',handleAddCLass));
+ $a.forEach($coin => $coin.addEventListener('click',handleAddCLass));
 
-//  $button.addEventListener('click',handleRemoveClass)
+ $button.addEventListener('click',handleRemoveClass)
  
-//  function handleAddCLass(e){
-//          $div.style.display = 'none'
-//  }
-//  function handleRemoveClass(e){
-//      $div.style.display = 'block'
-//  }
+ function handleAddCLass(e){
+         $div.style.display = 'none'
+ }
+ function handleRemoveClass(e){
+     $div.style.display = 'block'
+ }
 
 
  //O MODAL
 
-//  function autoPlayYouTubeModal() {
-//     var triggerOpen = $("body").find('[data-tagVideo]');
-//     triggerOpen.click(function() {
-//       var theModal = $(this).data("bs-target"),
-//         videoSRC = $(this).attr("data-tagVideo"),
-//         videoSRCauto = videoSRC + "?autoplay=1";
-//       $(theModal + ' iframe').attr('src', videoSRCauto);
-//       $(theModal + ' button.btn-close').click(function() {
-//         $(theModal + ' iframe').attr('src', videoSRC);
-//       });
-//     });
-//   }
-  
-
+ function autoPlayYouTubeModal() {
+    var triggerOpen = $("body").find('[data-tagVideo]');
+    triggerOpen.click(function() {
+      var theModal = $(this).data("bs-target"),
+        videoSRC = $(this).attr("data-tagVideo"),
+        videoSRCauto = videoSRC + "?autoplay=1";
+      $(theModal + ' iframe').attr('src', videoSRCauto);
+      $(theModal + ' button.btn-close').click(function() {
+        $(theModal + ' iframe').attr('src', videoSRC);
+      });
+    });
+  }
